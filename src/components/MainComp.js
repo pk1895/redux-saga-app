e4f1f184dddff739e75cb1da8d6868a6e3d62244
Prasad Khanapure <pnkhanapure@gmail.com>
2022-08-09 00:00:55 +0530

    changed added

---
+++ src/components/MainComp.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Counter from "./Counter";
+
+const MainComp = () => {
+  const voters = ["Lisa Harre", "Andrei Golosov"];
+
+  return (
+    <div>
+      <h2> Total Votes: </h2>
+    </div>
+  );
+};
+
+export default MainComp;
